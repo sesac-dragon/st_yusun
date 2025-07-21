95e87838fcc4f3a452938413c6dc60076b40f8c4
yusun <1223yusun@gmail.com>
2025-07-21 16:06:07 +0900

    second commit

--- a.py
+++ a.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import os
+
 
 st.title('베포 테스트')
-st.write('???')
+st.write('베포 테스트용 앱입니다.')
+
+st.write('자동입니까?')
+
+api_key  = os.environ.get("API_SECRET_KEY")
+
+st.write(f'api key = {api_key}')
